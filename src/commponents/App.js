403f17d41fc7cbd40a5f52c30bc7da49251096b5
Nikhil anand <Nikhil anand>
2020-09-15 12:02:19 +0530

    implemented routing

--- src/commponents/App.js
+++ src/commponents/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function Some() {
+  return <div>Some</div>;
+}
+
 import { SignIn } from "./";
 class App extends Component {
   render() {
     return (
       <div>
-        <SignIn />
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/some" component={Some} />
+        </Switch>
       </div>
     );
   }
