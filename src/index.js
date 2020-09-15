2aa961656c5d780f153834d8f054ed9df8da9d4a
Nikhil anand <Nikhil anand>
2020-09-15 12:13:23 +0530

    solved error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { App } from "./commponents";
 
-ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(
   <BrowserRouter>
     <App />
